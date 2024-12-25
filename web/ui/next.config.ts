dea31e4e3cdc2aef02ab38a6d6ad6aa82ff0dade
jeryfan <63136443+jeryfan@users.noreply.github.com>
2024-12-25 19:58:23 +0800

    action

--- web/ui/next.config.ts
+++ web/ui/next.config.ts
@@ -2,7 +2,12 @@ import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
     /* config options here */
-    output: "export"
+    output: "export",
+    basePath: "/one",
+    assetPrefix: "/one",
+    images: {
+        unoptimized: true,
+    },
 }
 
 export default nextConfig
