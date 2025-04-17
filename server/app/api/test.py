6b2a05faba52719ed25aabe75542b846252209ae
jeryfan <1295448630@qq.com>
2025-04-17 23:35:35 +0800

    fix

--- server/app/api/test.py
+++ server/app/api/test.py
@@ -1,10 +1,9 @@
 from fastapi import APIRouter
-from app.tasks.test_task import test_task
+
 
 router = APIRouter(prefix="/test")
 
 
 @router.get("/test")
 async def test():
-    task = test_task.delay()
-    return {"msg": "test", "task_id": task.id}
+    return {"msg": "test"}
